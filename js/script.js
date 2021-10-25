d0819e343b0fd24b3242ee0f32b62cc0e4c97548
Таалайбеков Эржан <erzanimanov@gmail.com>
2021-10-25 14:43:50 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$('.testimonials__box-slick').slick({
+    centerMode: true,
+    infinite: false,
+    // autoplay: true,
+    dots: true,
+    arrows:true,
+    slidesToShow: 3,
+    slidesToScroll: 3
+});
